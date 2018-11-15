54384e5329345ca1d21539a3d91037fdccfea9a6
Ramon Pereira <www.ramonthebest@gmail.com>
2018-11-15 11:57:35 -0200

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div>
+        Opa
+      </div>
+    );
+  }
+}
